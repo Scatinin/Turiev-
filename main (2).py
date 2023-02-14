d2dbdcf0806440aa0dc4a6d4f30ffed870724035
Scatinin <117991095+Scatinin@users.noreply.github.com>
2023-02-14 10:31:22 +0300

    №2 14.02.23

---
+++ main (2).py
@@ -0,0 +1,28 @@
+#1
+a=float(input())
+b=float(input())
+#2
+if (a > b) and (a+10):
+    print("True")
+    
+elif (a > b) and (a*10):
+    print("True")
+
+elif (a < b) and (b+10):
+    print("False")
+
+else: (a < b) and (b*10)
+    
+#3 
+#2
+if (a > b) or (a+10):
+    print("True")
+    
+elif (a > b) or (a*10):
+    print("True")
+
+elif (a < b) or (b+10):
+    print("False")
+
+else: (a < b) or (b*10)
+    
