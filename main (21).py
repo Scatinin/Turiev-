c819d6546456f046e6a946b5c2ebc1fea3bbf39a
Scatinin <117991095+Scatinin@users.noreply.github.com>
2023-04-11 09:57:11 +0300

    11.04.23 vvvv

---
+++ main (21).py
@@ -0,0 +1,7 @@
+#1
+a = int(input())
+b = 2
+while a > (b**0.5):
+    print(b**0.5)
+    b = b + 1
+print(a)
---
+++ main (23).py
@@ -0,0 +1,9 @@
+#3
+a = int(input())
+b = int(input())
+if a<b:
+    while a < b:
+        b = b - 1 
+        print(b)
+else:
+    print("уууу")
