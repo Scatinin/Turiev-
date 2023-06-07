c0bd32fa17e305e10519e7fff8e85ec4f6301a61
Scatinin <117991095+Scatinin@users.noreply.github.com>
2023-06-07 19:35:57 +0300

    долг

--- main (1).py
+++ main (1).py
@@ -1,10 +1,74 @@
-hours = float(input("Введите количество отработанных часов: "))
-credit = float(input("Введите остаток по кредиту: "))
-food_money = float(input("Введите количество денег на еду: "))
+#Задачи на цикл с параметром
+#1
+a=25.4
+b=10
+while b<23:
+   c=a*b
+   print(b,"дюйма=",c)
+   b=b+1
+#2
+a=int(input("Введите курс доллара: "))
+b=1
+while b<21:
+    c=a*b
+    print(b, "Доллоров", "=",c,"руб.")
+    b=b+1
+#3
+b=1
+while b<21:
+    b=b+1
+    print("e^",b)
+#44
+a=1.1
+while a<22.1:
+    a=a+1
+    print(a)
+#5
+a=2.0
+while a<=2.9:
+    a=a+0.1
+    print(a)
+#6
+a=0
+while a<30:
+    a=a+2
+    print(a)
+#7
+a=1
+while a<30:
+    print(a)
+    a=a+2
+#8
+a=2
+while a<22:
+    b=a**0.5
+    print(b)
+    a=a+2
+#9
+a=int(input("Введите цену 1 кг конфет: "))
+b=1
+while b<11:
+    c=a*b
+    print(c,'р', "За",b, "кг конфет")
+    b=b+1
+#10
+x=2
+while x<21:
+    f=2*x
+    z=8*f**3-f
+    print(z)
+    x=x+1
 
-salary = (200 * hours / 2 ** 3) + hours
 
-if salary >= credit + food_money:
-    print("Часов хватает. Можно отдохнуть")
-else:
-    print("Часов не хватает. Придется работать")
+
+
+
+
+
+
+
+
+
+
+
+
